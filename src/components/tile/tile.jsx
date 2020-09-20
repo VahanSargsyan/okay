f7da02f433a70ead2055a9e6c7a4b441569ab455
Vahan Sargsyan <vahansargsyan1310@gmail.com>
2020-09-20 15:43:26 +0400

    [add] Tile component

--- src/components/tile/tile.jsx
+++ src/components/tile/tile.jsx
@@ -1,3 +1,14 @@
 import React from "react";
+import style from "./tile.module.scss"
 
-export default ({color, number}) => <div className="root"></div>
+const Tile = ({color, number}) => {
+  const colorMap = {
+    blue: 0,
+    red: 13,
+    yellow: 26,
+    black: 39
+  };
+  const tileNumber = colorMap[color] + number - 1;
+  return <div className={style[`tile-${tileNumber}`]} onDragStart={(e) => console.log(e)}/>
+};
+export default Tile;
--- src/pages/game-room/game-room.jsx
+++ src/pages/game-room/game-room.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Tile from "../../components/tile/tile";
 
-export default () => {
-    return (
-        <div className="game-room">
-            <p>a tat at</p>
-        </div>
-    );
-}
+const GameRoom = () => {
+  return (
+    <div className="game-room">
+      <p>a tat at</p>
+      <Tile color="black" number={10}/>
+      <Tile color="blue" number={11}/>
+    </div>
+  );
+};
+
+export default GameRoom;
